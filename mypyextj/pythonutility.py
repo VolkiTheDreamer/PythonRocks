a6c74c36b7c7472b65247c7471cf3cd3d467767f
Volkan Yurtseven <volkan.yurtseven@hotmail.com>
2022-05-06 14:21:03 +0300

    Update pythonutility.py

--- mypyextj/pythonutility.py
+++ mypyextj/pythonutility.py
@@ -1,40 +1,44 @@
 from __future__ import print_function
 import inspect
-import os, sys, site
+import sys
 import functools
 import time
-from forbiddenfruit import curse
+import subprocess
+from IPython.display import Audio
+from IPython.core.interactiveshell import InteractiveShell
+
 
 try:
     import __builtin__
 except ImportError:
     import builtins as __builtin__
 
-# *************************************************************************************************************
-#Module level methods
-
-    
-def lineno():
-    previous_frame = inspect.currentframe().f_back.f_back
-    (filename, line_number, function_name, lines, index) = inspect.getframeinfo(previous_frame)
-    return (line_number, lines)
-    #return inspect.currentframe().f_back.f_back.f_lineno, str(inspect.currentframe().f_back)
 
 def printy(*args, **kwargs):
-    print(lineno(),"\n----------")
+    """
+        prints the results along with the line number in a cell containing  multiple print statement to prevent confusions.
+    """
+    def lineno():
+        previous_frame = inspect.currentframe().f_back.f_back
+        (filename, line_number, function_name, lines, index) = inspect.getframeinfo(previous_frame)
+        return (line_number, lines[0][:-1])
+
+    print(lineno(), "\n----------")
     print(*args, **kwargs)
     print(" ",end="\n")
 
 
 def timeElapse(func):
     """
+        alternative to %time/timeit. In case of using an IDE apart from jupyter.
+        
         usage:
-        @timeElapse
-        def somefunc():
-            ...
-            ...
+            @timeElapse
+            def somefunc():
+                ...
+                ...
 
-        somefunc()
+            somefunc()
     """
     @functools.wraps(func)
     def wrapper(*args,**kwargs):
@@ -44,35 +48,7 @@ def timeElapse(func):
         finito=time.time()
         print("Time elapsed:{}".format(finito-start))
         return value
-    return wrapper    
-
-
-def multioutput(type="all"):
-    from IPython.core.interactiveshell import InteractiveShell
-    InteractiveShell.ast_node_interactivity = type
-    
-def scriptforReload():
-    print("""
-    %load_ext autoreload
-    %autoreload 2""")
-   
-def scriptforTraintest():
-    print("X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=42)")
-    
-def scriptForCitation():
-    print("""<p style="font-size:smaller;text-align:center">Görsel <a href="url">bu sayfadan</a> alınmıştır</p>""")
-    
-def pythonSomeInfo():
-    print("system packages folder:",sys.prefix, end="\n\n")
-    print("pip install folder:",site.getsitepackages(), end="\n\n")    
-    print("python version:", sys.version, end="\n\n")
-    print("executables location:",sys.executable, end="\n\n")
-    print("pip version:", os.popen('pip version').read(), end="\n\n")
-    pathes= sys.path
-    print("Python pathes")
-    for p in pathes:
-        print(p)
-
+    return wrapper     
 
 def showMemoryUsage():
     dict_={}
@@ -88,40 +64,29 @@ def readfile(path,enc='cp1254'):
     with io.open(path, "r", encoding=enc) as f:
         return f.read()
 
-def getFirstItemFromDictionary(dict_):
-    return next(iter(dict_)),next(iter(dict_.values()))
-        
 
-
-def removeItemsFromList(self,list2,inplace=True):    
+def isLatestVersion(packagename):
     """
-        Extension method for list type. Removes items from list2 from list1.
-        First, forbiddenfruit must be installed via https://pypi.org/project/forbiddenfruit/
-    """    
-    if inplace:
-        for x in set(list2):
-            self.remove(x)
-        return self
-    else:
-        temp=self.copy()
-        for x in set(list2):
-            temp.remove(x)
-        return temp
-    
-curse(list, "removeItemsFromList", removeItemsFromList)
+        Provides the packagename without quotation marks and import the package in advance
+    """   
+    try:        
+        name=str(packagename.__name__)    
+        latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '{}==random'.format(name)], capture_output=True, text=True))
+        latest_version = latest_version[latest_version.find('(from versions:')+15:]
+        latest_version = latest_version[:latest_version.find(')')]
+        latest_version = latest_version.replace(' ','').split(',')[-1]
 
+        current_version = str(subprocess.run([sys.executable, '-m', 'pip', 'show', '{}'.format(name)], capture_output=True, text=True))
+        current_version = current_version[current_version.find('Version:')+8:]
+        current_version = current_version[:current_version.find('\\n')].replace(' ','') 
 
-def containsLike(self,what):    
-    """
-        ....
-    """    
-    for item in set(self):
-        if what in str(item):
-            return True
-    else:
-        return False    
-    
-curse(list, "containsLike", containsLike)
-
+        if latest_version == current_version:
+            print(name+", "+packagename.__version__+", True")
+        else:
+            print(name+", "+packagename.__version__+", False")
+    except NameError:
+        print("You should import the package first")
+    except:
+        print("You should import the package first.")
 
 
