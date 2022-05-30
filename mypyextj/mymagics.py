4be2e30c7f2614b1d4481994b35fd0180fe4a937
Volkan Yurtseven <volkan.yurtseven@hotmail.com>
2022-05-30 16:39:58 +0300

    Update mymagics.py

--- mypyextj/mymagics.py
+++ mypyextj/mymagics.py
@@ -9,6 +9,16 @@ from IPython.display import Audio
 from playsound import playsound
 import winsound
 import inspect
+from __future__ import print_function
+import inspect
+import functools
+import time
+import subprocess
+
+try:
+    import __builtin__
+except ImportError:
+    import builtins as __builtin__
 
 #1-this file is executed on startup as it is in C:\Users\username\.ipython\profile_default\startup
 
@@ -58,9 +68,107 @@ def mgc_beep(line, cell=None):
 
 @register_line_magic  
 def mgc_nbsinppet(line):
+    """
+        Converts a cell content,which was assigned to a string variable, into a nbextensions snippet-menu-compatible format.
+    """
     temp=[]
     for x in eval(line).split("\n"):     
         rep=x.replace("\"","'")
         temp.append("\""+rep+"\"")
 
     print(",\n".join(temp))    
+
+@register_cell_magic
+def showMemoryUsage(line=None,cell=None):
+    dict_={}
+    global_vars = list(globals().items())
+    for var, obj in global_vars:
+        if not var.startswith('_'):
+            dict_[var]=sys.getsizeof(obj)
+            
+    final={k: v for k, v in sorted(dict_.items(), key=lambda item: item[1],reverse=True)}    
+    print(final)
+    
+@register_line_magic    
+def read_filecontent(line,enc='cp1254'):
+    with open(line, "r", encoding=enc) as f:
+        return f.read()
+
+@register_line_magic  
+def isLatestVersion(line):
+    """
+        Provides the packagename without quotation marks and import the package in advance
+    """   
+    packagename=line
+    try:        
+        name=str(packagename.__name__)    
+        latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '{}==random'.format(name)], capture_output=True, text=True))
+        latest_version = latest_version[latest_version.find('(from versions:')+15:]
+        latest_version = latest_version[:latest_version.find(')')]
+        latest_version = latest_version.replace(' ','').split(',')[-1]
+
+        current_version = str(subprocess.run([sys.executable, '-m', 'pip', 'show', '{}'.format(name)], capture_output=True, text=True))
+        current_version = current_version[current_version.find('Version:')+8:]
+        current_version = current_version[:current_version.find('\\n')].replace(' ','') 
+
+        if latest_version == current_version:
+            print(name+", "+packagename.__version__+", True")
+        else:
+            print(name+", "+packagename.__version__+", False")
+    except NameError:
+        print("You should import the package first")
+    except:
+        print("You should import the package first.")
+    
+
+
+
+#*****************************NON-MAGICS************************************
+#THESE WILL BE READY AS SOON AS JUPYTER LAUNCHES AS THIS FILE IS IN STARTUP PATH
+# 
+def printy(*args, **kwargs):
+    """
+        prints the results along with the line number in a cell containing  multiple print statement to prevent confusions.
+    """
+    def lineno():
+        previous_frame = inspect.currentframe().f_back.f_back
+        (filename, line_number, function_name, lines, index) = inspect.getframeinfo(previous_frame)
+        return (line_number, lines[0][:-1])
+
+    print(lineno(), "\n----------")
+    print(*args, **kwargs)
+    print(" ",end="\n")
+
+
+def timeElapse(func):
+    """
+        alternative to %time/timeit. In case of using an IDE apart from jupyter.
+        
+        usage:
+            @timeElapse
+            def somefunc():
+                ...
+                ...
+
+            somefunc()
+    """
+    @functools.wraps(func)
+    def wrapper(*args,**kwargs):
+        start=time.time()
+        value=func(*args,**kwargs)
+        func()
+        finito=time.time()
+        print("Time elapsed:{}".format(finito-start))
+        return value
+    return wrapper     
+
+def showMemoryUsage():
+    dict_={}
+    global_vars = list(globals().items())
+    for var, obj in global_vars:
+        if not var.startswith('_'):
+            dict_[var]=sys.getsizeof(obj)
+            
+    final={k: v for k, v in sorted(dict_.items(), key=lambda item: item[1],reverse=True)}    
+    print(final)
+    
